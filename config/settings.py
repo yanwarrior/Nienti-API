8639f7b3f84f1e0f49eb249e445b7c512cb26b4c
Kong Bayan <yanwarsolah@gmail.com>
2020-06-27 15:20:00 +0700

    Menambahkan carts package

--- config/settings.py
+++ config/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS += [
     'suppliers',
     'customers',
     'products',
+    'carts',
 ]
 
 
--- products/serializers.py
+++ products/serializers.py
@@ -13,3 +13,5 @@ class ProductSerializer(serializers.ModelSerializer):
             'price',
             'stock',
         ]
+
+
