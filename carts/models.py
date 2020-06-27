558ff769d2efa38b40728df0a78f2122efb8510a
Kong Bayan <yanwarsolah@gmail.com>
2020-06-28 06:55:42 +0700

    Migrasi cart model

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from products.models import Product
+from utils.models import Timestamp
+
+
+class Cart(Timestamp):
+    user = models.ForeignKey(User, related_name='user_carts', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='product_carts', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    unit = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.product.name
