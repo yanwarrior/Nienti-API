ae080b405fb1ae3ddc8c31bd5a1209fa1bcfadf3
Kong Bayan <yanwarsolah@gmail.com>
2020-07-01 01:18:11 +0700

    Menambahkan invoice action di SaleViewSet.

--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,8 @@
 from django.db import transaction
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from carts.models import Cart
 from sales.models import Sale, Item
@@ -42,3 +44,129 @@ class SaleViewSet(viewsets.ModelViewSet):
 
         Item.objects.bulk_create(item_set)
         carts.delete()
+
+    @action(detail=True, methods=['POST'])
+    def invoice(self, request, pk=None):
+        sale = self.get_object()
+        items = sale.sale_items.all()
+
+        column_sale = [
+            {
+                'text': sale.sale_number,
+                'style': 'subheader'
+            },
+            {
+                'text': sale.sale_date,
+                'style': 'textMuted',
+            }
+        ]
+
+        column_customer = [
+            {
+                'alignment': 'right',
+                'text': sale.customer.name,
+                'style': 'subheader'
+            },
+            {
+                'alignment': 'right',
+                'text': sale.customer.phone,
+                'style': 'textMuted'
+            },
+            {
+                'alignment': 'right',
+                'text': sale.customer.address,
+                'style': 'textMuted'
+            },
+        ]
+
+        item_header = [
+            {
+                'text': 'Product',
+                'style': 'tableHeader',
+                'fillColor': 'green',
+                'color': 'white'
+
+            },
+            {
+                'text': 'Price',
+                'style': 'tableHeader',
+                'fillColor': 'green',
+                'color': 'white'
+
+            },
+            {
+                'text': 'Quantity',
+                'style': 'tableHeader',
+                'fillColor': 'green',
+                'color': 'white'
+
+            },
+            {
+                'text': 'Subtotal',
+                'style': 'tableHeader',
+                'fillColor': 'green',
+                'color': 'white'
+
+            }
+        ]
+
+        item_body = []
+        for item in items:
+            item_body.append([item.name, "${:,.2f}".format(item.price), item.quantity, "${:,.2f}".format(item.subtotal)])
+
+        doc_def = {
+            'content': [
+                {
+                    'text': 'Sales Order',
+                    'alignment': 'center',
+                    'style': 'header'
+                },
+                '\n\n',
+                {
+                    'alignment': 'justify',
+                    'columns': [column_sale, column_customer],
+                },
+                '\n',
+                {
+                    'style': 'tableExample',
+                    'table': {
+                        'widths': ['*'] * 4,
+                        'body': [
+                            item_header,
+                            *item_body
+                        ]
+                    }
+                }
+            ],
+            'styles': {
+                'header': {
+                    'fontSize': 20,
+                    'bold': True
+                },
+                'subheader': {
+                    'fontSize': 15,
+                    'bold': True
+                },
+                'textMuted': {
+                    'fontSize': 12,
+                    'color': 'gray'
+                },
+                'quote': {
+                    'italics': True
+                },
+                'small': {
+                    'fontSize': 8
+                },
+                'tableExample': {
+                    'margin': [0, 5, 0, 15],
+
+                },
+                'tableHeader': {
+                    'bold': True,
+                    'fontSize': 13,
+                    'color': 'black',
+                }
+            },
+        }
+
+        return Response(doc_def)
